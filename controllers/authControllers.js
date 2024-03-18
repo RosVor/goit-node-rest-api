7d614d6d53672d14f3cc2100103878035a41b707
RosVor <ros.vorokh@gmail.com>
2024-03-18 22:31:17 +0200

    git

---
+++ controllers/authControllers.js
@@ -0,0 +1,71 @@
+import ctrlWrapper from "../helpers/ctrlWrapper.js";
+import HttpError from "../helpers/HttpError.js";
+import jwt from "jsonwebtoken";
+import {registerUser, findUser, validatePassword, updateUser,} from "../services/authServices.js";
+
+const { JWT_SECRET } = process.env;
+
+const register = async (req, res) => {
+  const { email } = req.body;
+  const user = await findUser({ email });
+  if (user) {
+    throw HttpError(409, "Email is already in use");
+  }
+  const newUser = await registerUser(req.body);
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await findUser({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const comparePassword = await validatePassword(password, user.password);
+  if (!comparePassword) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const { _id: id } = user;
+  const payload = { id };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+  await updateUser({ _id: id }, { token });
+  res.json({ token });
+};
+
+const getCurrent = async (req, res) => {
+  const { email, subscription } = req.user;
+  res.json({
+    email,
+    subscription,
+  });
+};
+
+const logOut = async (req, res) => {
+  const { _id } = req.user;
+  await updateUser({ _id }, { token: "" });
+
+  res.status(204).send();
+};
+
+const changeSubscription = async (req, res) => {
+  const { _id } = req.user;
+  const { subscription } = req.body;
+  const updatedUser = await updateUser(_id, { subscription });
+  if (!updatedUser) {
+    return res.status(400).json({ error: "Invalid subscription value" });
+  }
+  res.json({ message: "Successfully Updated" });
+};
+
+export default {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logOut: ctrlWrapper(logOut),
+  changeSubscription: ctrlWrapper(changeSubscription),
+};
