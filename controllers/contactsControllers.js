030bbe2db427cf5341bbdb770e678f19d506a89e
RosVor <ros.vorokh@gmail.com>
2024-03-23 03:56:21 +0200

    lastCheck

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,58 +1,74 @@
+import HttpError from "../helpers/HttpError.js";
 import ctrlWrapper from "../helpers/ctrlWrapper.js";
-import HttpError from '../helpers/HttpError.js';
+
 import contactsServices from "../services/contactsServices.js";
 
-  const getAllContacts = async (_,res) => {
-    const result = await contactsServices.listContacts();
-    res.json(result);
-  };
-  
-  const getOneContact = async (req, res) => {
-    const { id } = req.params;
-    const result = await contactsServices.getContactById(id);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json(result);
-  };
-  
-  const deleteContact = async (req, res) => {
-    const { id } = req.params;
-    const result = await contactsServices.removeContact(id);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json({ message: "Delete succesed" });
-  };
-  
-  const createContact = async (req, res) => {
-    const result = await contactsServices.addContact(req.body);
-    res.status(201).json(result);
-  };
-  
-  const updateContact = async (req, res) => {
-    const { id } = req.params;
-    const result = await contactsServices.updateContactById(id, req.body);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json(result);
-  };
-  
-  const updateStatusContact = async (req, res) => {
-    const { id } = req.params;
-    const result = await contactsServices.updateContactById(id, req.body);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json(result);
-  };
+const getAllContacts = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  const filter = favorite ? { owner, favorite: true } : { owner };
+  const result = await contactsServices.listContacts(filter, { skip, limit });
+  res.json(result);
+};
 
-  export default {
-    getAllContacts: ctrlWrapper(getAllContacts),
-    getOneContact: ctrlWrapper(getOneContact),
-    deleteContact: ctrlWrapper(deleteContact),
-    createContact: ctrlWrapper(createContact),
-    updateContact: ctrlWrapper(updateContact),
-    updateStatusContact: ctrlWrapper(updateStatusContact),
-  };
+const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const { _id: owner } = req.user;
+  const result = await contactsServices.getOneContactById({ _id: id, owner });
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const { _id: owner } = req.user;
+  const result = await contactsServices.removeOneContact({ _id: id, owner });
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json({ message: "Delete succes" });
+};
+
+const createContact = async (req, res) => {
+  const { _id: owner } = req.user;
+  const result = await contactsServices.addContact({ ...req.body, owner });
+  res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const { _id: owner } = req.user;
+  const result = await contactsServices.updateOneContactById(
+    { _id: id, owner },
+    req.body
+  );
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { id } = req.params;
+  const { _id: owner } = req.user;
+  const result = await contactsServices.updateOneContactById(
+    { _id: id, owner },
+    req.body
+  );
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+export default {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getOneContact: ctrlWrapper(getOneContact),
+  deleteContact: ctrlWrapper(deleteContact),
+  createContact: ctrlWrapper(createContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,14 +1,15 @@
 import Joi from "joi";
+import { phoneRegex, emailRegex } from "../constants/userConstants.js";
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .messages({ "string.pattern.base": "XXXXX@XXX.com/XXXXX@XXX.net" })
+    .email(emailRegex)
+    .messages({ "string.pattern.base": `Incorrect e-mail format` })
     .required(),
   phone: Joi.string()
-    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
-    .messages({ "string.pattern.base": "(XXX) XXX-XXXX" })
+    .pattern(phoneRegex)
+    .messages({ "string.pattern.base": `Incorrect phone format` })
     .required(),
   favorite: Joi.boolean(),
 });
@@ -16,16 +17,16 @@ export const createContactSchema = Joi.object({
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(30),
   email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .messages({ "string.pattern.base": "XXXXX@XXX.com/XXXXX@XXX.net" }),
+    .email(emailRegex)
+    .messages({ "string.pattern.base": `Incorrect e-mail format` }),
   phone: Joi.string()
-    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
-    .messages({ "string.pattern.base": "(XXX) XXX-XXXX" }),
+    .pattern(phoneRegex)
+    .messages({ "string.pattern.base": `Incorrect phone format` }),
   favorite: Joi.boolean(),
 })
   .min(1)
   .message("Body must have at least one field");
 
-  export const updateFavoriteSchema = Joi.object({
+export const updateFavoriteSchema = Joi.object({
   favorite: Joi.boolean().required(),
 });
